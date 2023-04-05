acdf8827f1f2ad95c02508b87b64748dbc77be3b
MatteoGaloni <mtgaloni@gmail.com>
2023-04-05 15:56:34 +0200

    add  button

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-alert("First Test");
+let nome = prompt("Qual è il tuo nome?");
+console.log(`L'utente dichiara che il suo nome è ${nome}`);
+let cognome = prompt("Qual è il tuo cognome?");
+console.log(`L'utente dichiara che il suo cognome è ${cognome}`);
+let colorePreferito = prompt("Qual è il tuo colore preferito?");
+console.log(`L'utente dichiara che il suo colore preferito è ${colorePreferito}`);
+
+
+document.getElementById("name").innerText = nome;
+document.getElementById("surname").innerText = cognome;
+document.getElementById("color").innerText = colorePreferito;
